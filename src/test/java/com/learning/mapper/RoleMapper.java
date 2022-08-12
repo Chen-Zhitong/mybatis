d9a3dfeada13955e30d1ce9ca93abb3372b9b977
Sikoay <chen-zhitong@Outlook.com>
2022-08-12 22:28:07 +0800

    完成源码调用准备

---
+++ src/test/java/com/learning/mapper/RoleMapper.java
@@ -0,0 +1,14 @@
+package com.learning.mapper;
+
+import com.learning.po.Role;
+
+/**
+ * @author chenzt
+ * @date 2022/8/12
+ */
+public interface RoleMapper {
+    public Role getRole(Long id);
+    public Role findRole(String roleName);
+    public int deleteRole(Long id);
+    public int insertRole(Role role);
+}
