f5c38284ee21da595360d67ccb7be462bf8eb24f
Sikoay <chen-zhitong@Outlook.com>
2022-08-13 16:33:08 +0800

    docs: 注释 JdbcType

--- src/main/java/org/apache/ibatis/type/JdbcType.java
+++ src/main/java/org/apache/ibatis/type/JdbcType.java
@@ -20,18 +20,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 使用这个枚举类型代表JDBC中的数据类型, 在这个枚举类型汇总定义了TYPE_CODE字段,
+ * 记录了JDBC类型在java.sql.Types中相应的常量编码,
+ * 并通过一个静态集合 codeLookup 维护了常量编码与JdbcType之间的关系
+ *
  * @author Clinton Begin
- */
-
-/**
- * JDBC类型枚举
  */
 public enum JdbcType {
     /*
      * This is added to enable basic support for the
      * ARRAY data type - but a custom type handler is still required
      */
-    //就是包装一下java.sql.Types
     ARRAY(Types.ARRAY),
     BIT(Types.BIT),
     TINYINT(Types.TINYINT),
@@ -68,6 +67,7 @@ public enum JdbcType {
     private static Map<Integer, JdbcType> codeLookup = new HashMap<Integer, JdbcType>();
 
     //一开始就将数字对应的枚举型放入hashmap
+    // 通过静态集合codeLookup 维护了常量编码与jdbcType之间的对应关系
     static {
         for (JdbcType type : JdbcType.values()) {
             codeLookup.put(type.TYPE_CODE, type);
